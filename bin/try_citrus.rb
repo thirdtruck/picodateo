1a3dff5e8b1aca5e40014238420672a487c558a5
J. C. Holder <github.com@thirdtruck.org>
2016-05-10 23:27:08 -0400

    WIP: Fix citrus code to capture multiple scenes and commands properly.

--- bin/try_citrus.rb
+++ bin/try_citrus.rb
@@ -6,15 +6,36 @@ Actor = Struct.new(:name) do
 end
 
 Scene = Struct.new(:name, :commands) do
+  def to_s
+    commands.map {|c| "{#{c}}"}.join(',')
+  end
 end
 
 Speech = Struct.new(:actor, :text) do
+  def to_s
+    # TODO: Add explicit escape sequences, if necessary
+    escaped_text = text.inspect
+    if actor == :_narrator
+      "type=\"narration\",text=#{escaped_text}"
+    else
+      "type=\"speech\",speaker=\"#{actor}\",text=#{escaped_text}"
+    end
+  end
 end
 
-scenes = {}
+Game = Struct.new(:scenes) do
+  def to_s
+    scene_strings = []
+    scenes.each_pair do |name, scene|
+      scene_strings << "#{name}={#{scene}}"
+    end
+    "scenes={#{scene_strings.join(',')}}"
+  end
+end
 
 Citrus.load 'bin/picodateo'
 m = PicoDateo.parse File.read('game/scripts/citrus.dateo')
-binding.pry
-pp m.value
-puts
+cs = nil
+game = m.value
+#binding.pry
+puts game
